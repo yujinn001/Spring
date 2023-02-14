78219ef59c854f291891458a3c0aa6879a473690
yujinn001 <14rladbwls@naver.com>
2023-02-14 16:15:58 +0900

    230214 DI (xml Vs java)

---
+++ SpringBasicProject10_DI/src/main/java/com/sist/config/StudentConfig.java
@@ -0,0 +1,93 @@
+package com.sist.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.sist.dao.StudentDAO;
+import com.sist.myapp.MainClass;
+
+@Configuration
+public class StudentConfig {
+	/*<bean id="mc" class="com.sist.myapp.MainClass"
+		   p:dao-ref="dao" 
+		/>
+	*/
+	@Bean("mc")
+	public MainClass mainClass() throws Exception
+	{
+		MainClass mc=new MainClass();
+		mc.setDao(studentDAO());
+		return mc;
+	}
+	
+	/*
+	 * <bean id="ds" class="org.apache.commons.dbcp.BasicDataSource"
+	   p:driverClassName="oracle.jdbc.driver.OracleDriver"
+	   p:url="jdbc:oracle:thin:@localhost:1521:XE"
+	   p:username="hr"
+	   p:password="happy"	  
+	/>*/
+	@Bean("ds")
+	public BasicDataSource basicDataSource()
+	{
+		BasicDataSource ds=new BasicDataSource();
+		ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		ds.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		ds.setUsername("hr");
+		ds.setPassword("happy");
+		return ds;
+	}
+	
+	/*<!--  DataSource정보를 MyBatis로 전송 : SqlSessionFactoryBean -->
+	<bean id="ssf" class="org.mybatis.spring.SqlSessionFactoryBean"
+	
+	  p:configLocation="classpath:Config.xml"
+	  p:dataSource-ref="ds" => ssf.setDataSource(basicDataSource());
+	/>*/
+	@Bean("ssf")
+	public SqlSessionFactory sqlSessionFactory() throws Exception
+	{
+		SqlSessionFactoryBean ssf=new SqlSessionFactoryBean();
+		ssf.setDataSource(basicDataSource());
+		Resource res=new ClassPathResource("Config.xml");
+		ssf.setConfigLocation(res);
+		return ssf.getObject();
+	}
+	
+	/*<bean id="dao" class="com.sist.dao.StudentDAO"
+	  p:sqlSessionFactory-ref="ssf"
+	/>
+	 */
+	@Bean("dao")
+	public StudentDAO studentDAO() throws Exception
+	{
+		StudentDAO dao=new StudentDAO();
+		dao.setSqlSessionFactory(sqlSessionFactory());
+		return dao;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SpringBasicProject10_DI/src/main/java/com/sist/myapp/MainClass2.java
@@ -0,0 +1,10 @@
+package com.sist.myapp;
+// 자바로 환경 설정
+public class MainClass2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
